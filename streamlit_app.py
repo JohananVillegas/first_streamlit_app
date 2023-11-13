8b7f4a70ee78d0a680823f3425af954db883e952
JohananVillegas <150577010+JohananVillegas@users.noreply.github.com>
2023-11-12 23:57:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-#from urllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Moms New Healthy Diner')
 
@@ -26,20 +26,22 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
- #if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
- else:
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
 
-except URLError as e:
- streamlit.error()
+   # fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ #if not fruit_choice:
+    #streamlit.error("Please select a fruit to get information.")
+ #else:
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(frutyvice_response.json)
+
+  # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   # streamlit.dataframe(fruityvice_normalized)
+
+#except URLError as e:
+ #streamlit.error()
 
 #dont run anything past here while we troubleshoot 
-streamlit.stop()
+#streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
